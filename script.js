76fa74ff8a72684f60390611c0e35d5169870ed7
Omaalois <116834791+Omaalois@users.noreply.github.com>
2022-12-03 21:04:37 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let modal = document.getElementById("modal-bg");
+
+function openModal() {
+  modal.classList.add("open-modal");
+}
+
+function closeModal() {
+  modal.classList.remove("open-modal");
+}
+
+let popup = document.getElementById("modal_container");
+
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
+
+
+
+
